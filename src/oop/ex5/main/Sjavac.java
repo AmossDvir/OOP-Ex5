0007622e486c08b4f378a607c98fdd261ada1043
AmossDvir <amoss.dvir@mail.huji.ac.il>
2021-09-03 15:03:19 +0300

    First commit

---
+++ src/oop/ex5/main/Sjavac.java
@@ -0,0 +1,10 @@
+package oop.ex5.main;
+
+public class Sjavac {
+    /**
+     * Main method of the program.
+     * @param args: String array containing arguments.
+     */
+    public static void main(String[] args) {
+    }
+}
