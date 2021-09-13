aa9e01d62029dbf2083756da08c63c9b79f10fa4
macbook12 <miryam.buglayev@gmail.com>
2021-09-13 13:13:17 +0300

    block

--- src/syntaxchecking/methods/Method.java
+++ src/syntaxchecking/methods/Method.java
@@ -1,18 +1,13 @@
 package syntaxchecking.methods;
 
-import syntaxchecking.conditions.Condition;
 import syntaxchecking.methods.declarations.VoidDeclaration;
 import syntaxchecking.methods.exceptions.MethodException;
 import syntaxchecking.variables.Variable;
 import utilities.MethodsPair;
-import utilities.Pair;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-
-import static utilities.RegexExpressions.*;
 
 /**
  *
@@ -20,44 +15,23 @@ import static utilities.RegexExpressions.*;
 public class Method {
     private List<String> methodLines;
     private Map<String, Variable> variables;
-    private Map<String,List<String>> methodsList;
+    private Map<String, List<String>> methodsList;
 
-    public Method(List<String> methodLines,Map<String,List<String>> methodsList) {
+    public Method(List<String> methodLines, Map<String, List<String>> methodsList) {
         this.methodLines = methodLines;
         this.variables = new HashMap<>();
-        this.methodsList = methodsList ;
+        this.methodsList = methodsList;
     }
 
     /**
-     *
      * @return
      * @throws MethodException
      */
     public void analyze() throws MethodException {
         MethodsPair m = VoidDeclaration.analyzeDeclaration(methodLines.get(0), variables);
         // TODO: CONTINUE
-        for (String line:methodLines){
-            classifyLine(line);
-        }
 
-    }
 
-    /**
-     * Classifies a given line and invokes the corresponding method.
-     */
-    private void classifyLine(String line) throws MethodException {
-        Matcher lineMatcher = IF_WHILE_BLOCK_PATTERN.matcher(line);
-        if(lineMatcher.matches()){
-            Condition condition = new Condition(line,variables);
-            condition.checkCondition();
-
-        }
-        lineMatcher = METHOD_CALLING_BLOCK_PATTERN.matcher(line);
-        if(lineMatcher.matches()){
-            }
-        lineMatcher = RETURN_BLOCK_PATTERN.matcher(line);
-        if(lineMatcher.matches()){
-        }
     }
 
     /**
